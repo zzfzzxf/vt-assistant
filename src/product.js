eb5b259e4a9808fba9242f22400c04b40b5bb944
zzfzzxf <212026509+zzfzzxf@users.noreply.github.com>
2026-09-21 20:41:23 +0800

    Show current software version and last update in download section

--- src/product.js
+++ src/product.js
@@ -1,5 +1,8 @@
 export const product = {
   name: 'VT助手',
+  version: 'V7.0',
+  updatedAt: '2026-09-19',
+  updatedAtLabel: '2026.9.19',
   download: {
     url: './downloads/VT-Assistant-c6c616a54a03.7z',
     filename: 'VT助手.7z',
